c020b8dd3b4dcb4e5bdde137c5fd14823f3f28ee
eguzma <45185969+eguzma@users.noreply.github.com>
2019-03-19 19:27:29 -0600

    Add files via upload

---
+++ js/my-app.js
@@ -0,0 +1,75 @@
+// Initialize your app
+var myApp = new Framework7({
+	root:'#app',
+	theme: 'auto', //ios, md, auto
+    modalTitle: 'Framework7', 	//title for modals
+	panel: {
+    	swipe: 'left',
+  	},
+	cache:false,
+	pushState:false,
+	swipeBackPage:true,
+	material: true,
+	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/destino/',
+    	url: 'destino.html',
+    	name: 'destino',
+  		},
+		{
+		path: '/promocion/',
+    	url: 'promocion.html',
+    	name: 'promocion',
+  		},
+		{
+		path: '/listas/',
+    	url: 'listas.html',
+    	name: 'listas',
+  		},
+		{
+		path: '/cards/',
+    	url: 'cards.html',
+    	name: 'cards',
+  		},
+		{
+		path: '/tabs/',
+    	url: 'tabs.html',
+    	name: 'tabs',
+  		},
+		{
+		path: '/accordions/',
+    	url: 'accordions.html',
+    	name: 'accordions',
+  		}
+	]
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+
+var notificationWelcome = myApp.notification.create({
+  icon: '<i class="material-icons">info</i>',
+  title: '¡Bienvenido!',
+  subtitle: 'Aplicaciones con Framework7',
+  text: '',
+  closeButton: true,
+  closeTimeout: 3000,
+});
+$$('.open-welcome').on('click', function () {
+  notificationWelcome.open();
+});
+$$(window).on('load', function () {
+  notificationWelcome.open();
+});
+
+
+
+
+ 
+
